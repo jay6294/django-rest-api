fc5243091748ce7fc4245f8440b68f1864ae17f3
jay <mrityunjay.python@gmail.com>
2020-08-24 03:26:32 +0530

    added permission and filters

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, filters
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
 from profile_api import Serializer
 from profile_api import models
+from profile_api import permission
 
 
 class HelloApiViews(APIView):
@@ -81,3 +84,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """handle creating and updating profiles"""
     serializer_class = Serializer.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permission.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
